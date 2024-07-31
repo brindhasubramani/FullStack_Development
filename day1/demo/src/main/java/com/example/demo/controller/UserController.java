91d55f2f780e825a154257fb6a78f399720fc082
Brindha S <brindhasubramani2005@gmail.com>
2024-07-31 09:34:50 +0530

    Add files via upload

---
+++ day1/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package main.java.com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/postdetails")
+    public User postMethodName(@RequestBody User user) 
+    {
+        return userService.postDetails(user);
+    }
+    
+    @GetMapping("/details/{id}")
+    public User getMethodName(@PathVariable("id") int id) {
+        return userService.getDetails(id);
+    }
+    
+}
+
